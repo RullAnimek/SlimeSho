54036fead5c4c5c7220d32b63b00cf5aaaa0121b
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-13 16:52:43 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -9,7 +9,7 @@ handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   if (chat.antiLink && isGroupLink) {
     await m.reply(`*「 ANTI LINK 」*\n\nDetected *${await conn.getName(m.sender)}* you have sent the group link!\n\nSorry you will be kicked from this group byee!`)
     if (isAdmin) return m.reply('*Eh sorry you admins, you will not be kicked. hehe..*')
-    if (!isBotAdmin) return m.reply('*Bots are not admins, how can they kick people _-*')
+    if (!isBotAdmin) return m.reply('*Bots are not admins, how can they kick people hhhh*')
     let linkGC = ('https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat))
     let isLinkconnGc = new RegExp(linkGC, 'i')
     let isgclink = isLinkconnGc.test(m.text)
