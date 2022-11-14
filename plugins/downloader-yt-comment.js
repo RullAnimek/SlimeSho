03071850745a637c780fa5759aacc4b33bc5ef64
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-14 13:44:56 +0700

    Update downloader-yt-comment.js

--- plugins/downloader-yt-comment.js
+++ plugins/downloader-yt-comment.js
@@ -4,12 +4,12 @@ let handler = async (m, { conn, text }) => {
     avatar: await conn.getProfilePicture(m.sender).catch(_ => ''),
     comment: text,
     username: conn.getName(m.sender)
-  }), 'yt-comment.png', 'Here is your comment', m)
+  }), 'yt-comment.png', 'you comment ', m)
 }
 
-handler.help = ['ytcomment <comment>']
+handler.help = ['ytcomment [text]']
 handler.tags = ['maker']
-
 handler.command = /^(ytcomment)$/i
+handler.limit = true
 
 module.exports = handler
