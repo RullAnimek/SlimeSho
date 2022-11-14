e827a0ad10ad82d74ec9a21e68d6ee35e6fa581e
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-14 13:38:12 +0700

    Update database.js

--- plugins/database.js
+++ plugins/database.js
@@ -1,9 +1,11 @@
 let handler = async (m) => {
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-    m.reply(`*Jumlah database saat ini ${totalreg} user*`)
+    m.reply(`*Database: ${totalreg} user*`)
 }
 handler.help = ['database', 'user']
 handler.tags = ['info']
 handler.command = /^(database|jumlahdatabase|user)$/i
+handler.private = true
+
 module.exports = handler
