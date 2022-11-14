f2bef67b84023a62d1f3e59732143a4508ffb246
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-14 13:27:07 +0700

    Update darkjokes.js

--- plugins/darkjokes.js
+++ plugins/darkjokes.js
@@ -1,8 +1,9 @@
 let handler = async(m, { conn }) => {
-conn.sendFile(m.chat, global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY'), '', 'nih',m)
+conn.sendFile(m.chat, global.API('xteam', '/asupan/darkjoke', {}, 'APIKEY'), '', '🔦',m)
 }
 handler.help = ['darkjokes'|'dj']
 handler.tags = ['internet']
-handler.command = /^(dj|darkjokes)$/i
+handler.command = /^(darkjokes)$/i
+handler.limit = true
 
 module.exports = handler
