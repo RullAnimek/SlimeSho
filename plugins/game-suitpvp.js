09f8a7e607e8414bd23481032d65023f8c437116
RullAnimek <115926002+RullAnimek@users.noreply.github.com>
2022-11-14 13:34:15 +0700

    Update game-suitpvp.js

--- plugins/game-suitpvp.js
+++ plugins/game-suitpvp.js
@@ -12,7 +12,7 @@ let handler = async (m, { conn, usedPrefix }) => {
   if (Object.values(conn.suit).find(room => room.id.startsWith('suit') && [room.p, room.p2].includes(m.mentionedJid[0]))) throw `Orang yang kamu tantang sedang bermain suit bersama orang lain :(`
   let id = 'suit_' + new Date() * 1
   let caption = `
-_*SUIT PvP*_
+🏖️ Suit PvP 🏖️
 
 @${m.sender.split`@`[0]} menantang @${m.mentionedJid[0].split`@`[0]} untuk bermain suit
 
@@ -20,13 +20,13 @@ Silahkan @${m.mentionedJid[0].split`@`[0]}
 `.trim()
   let footer = `Ketik "terima/ok/gas" untuk memulai suit\nKetik "tolak/gabisa/nanti" untuk menolak`
   conn.suit[id] = {
-    chat: await conn.sendButton(m.chat, caption, footer, { 'button[0]': 'Terima', 'row[0]': 'ok', 'button[1]': 'Tolak', 'row[1]': 'tolak' }, m, { mentions: conn.parseMention(caption) }),
+    chat: await conn.sendButton(m.chat, caption, footer, { 'button[0]': 'Terima tantangan', 'row[0]': 'ok', 'button[1]': 'Tolak', 'row[1]': 'tolak' }, m, { mentions: conn.parseMention(caption) }),
     id: id,
     p: m.sender,
     p2: m.mentionedJid[0],
     status: 'wait',
     waktu: setTimeout(() => {
-      if (conn.suit[id]) conn.reply(m.chat, `_Waktu suit habis_`, m)
+      if (conn.suit[id]) conn.reply(m.chat, `_⏱️Waktu suit habis_`, m)
       delete conn.suit[id]
     }, timeout), poin, poin_lose, timeout
   }
